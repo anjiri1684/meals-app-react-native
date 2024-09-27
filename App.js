97ce9ddfd0913a362f55d5d48c2ff3fe6f4dc106
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-27 19:10:59 +0300

    "Updated App.js to use CategoriesScreen, added @react-navigation/native dependency, and modified app.json and package-lock.json files."

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import CategoriesScreen from "./screens/CategoryScreen";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ components/CategoryGridTile.js
@@ -0,0 +1,57 @@
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View style={[styles.gridItem]}>
+      <Pressable
+        style={(pressed) => [styles.button, pressed && styles.buttonPressed]}
+        android_ripple={{ color: "#ccc" }}
+      >
+        <View
+          style={[
+            styles.innerContainer,
+            {
+              backgroundColor: color,
+            },
+          ]}
+        >
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  buttonPressed: {
+    opacity: 0.2,
+  },
+  button: { flex: 1 },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
